47e2dbd2bd0ab259802a3beca8fe4d5bd582513b
pawan79797979 <pawan.pandey79797979@gmail.com>
2018-10-14 20:30:00 +0000

    shell script smoke

--- scripts/shell-script-prv.sh
+++ scripts/shell-script-prv.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-yum -y install nginx
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y update
+
+sudo yum -y install java-1.8.0-openjdk.x86_64 wget firewalld
+
+#Installing Tomcat 8
+cd /opt
+sudo wget http://mirrors.fibergrid.in/apache/tomcat/tomcat-8/v8.5.34/bin/apache-tomcat-8.5.34.tar.gz
+sudo tar xvfz apache-tomcat-8.5.34.tar.gz
+cd /opt/apache-tomcat-8.5.34/bin
+
+sudo chmod +x *
+sudo ./startup.sh
--- scripts/shell-script-pub.sh
+++ scripts/shell-script-pub.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-yum -y install httpd
 
+sudo yum -y install  https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y update
+
+#Installation Jenkins
+
+sudo yum -y install java-1.8.0-openjdk.x86_64 wget firewalld
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum -y install jenkins
+
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=8080/tcp
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --reload
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+# Install Ansible
+sudo yum -y install ansible
